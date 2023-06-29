9f00383dc33b75437706871a1c359f8122be180d
Mikael Saraiva <mikaelsaraiva@gmail.com>
2023-06-29 00:18:58 -0300

    Feat: Payment info Added the payment section to the form with corresponding validation.

--- star-wars-car-shop/src/components/checkout/validation.jsx
+++ star-wars-car-shop/src/components/checkout/validation.jsx
@@ -1,8 +1,14 @@
-const addressForm = document.querySelector('#address-info')
+// Componentes do formulario de informações pessoais
+
 const nameInput = document.querySelector('#name')
 const emailInput = document.querySelector('#email')
 const phoneInput = document.querySelector('#phone')
 const cpf_cnpjInput = document.querySelector('#cpf_cnpj')
+
+// Fomulario de informações de endereço e seus componentes
+
+const addressForm = document.querySelector('#address-info')
+
 const zipcodeInput = document.getElementById("zipcode")
 const addressInput = document.querySelector('#address')
 const numberInput = document.querySelector('#number')
@@ -11,6 +17,23 @@ const cityInput = document.querySelector('#city')
 const neighborhoodInput = document.querySelector('#neighborhood')
 const stateInput = document.querySelector('#state')
 
+// Componente de seleção do método de pagamento
+
+const paymentMethodSelect = document.querySelector('#paymeny-method')
+
+// Componente que tras informação do boleto
+
+const paymentSlip = document.querySelector('#payment-slip')
+
+// Fomulario de crédito e seus componentes
+
+const creditForm = document.querySelector('#credit')
+
+const cardNumberInput = document.querySelector('#card-number')
+const cardCreditLogo = document.querySelector('#card-credit-logo')
+const expireInput = document.querySelector('#expire')
+const nameOnCardInput = document.querySelector('#name-on-card')
+const securityCodeInput = document.querySelector('#security-code')
 
 // Função para desabilitar/habilitar as caixas de texto do fomulario de endereço
 
@@ -160,6 +183,129 @@ function isCPFCNPJValid(cpf_cnpj) {
     return true;
 }
 
+// Verifica se é uma data correta e dentro do prazo de validade
+
+function isDateValid(date) {
+    const today = new Date()
+    const expirationDay = parseInt(date[0])
+    const expirationMonth = parseInt(date[1])
+    const expirationYear = parseInt(date[2])
+
+    if (expirationYear > today.getFullYear()) { // Verifica se é um ano valido
+
+        return true
+    }
+
+    if (expirationYear === today.getFullYear) {
+        if (expirationMonth > today.getMonth) {
+
+            return true
+
+        } else if (expirationMonth === today.getMonth) {
+            switch (expirationMonth) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12: {
+                    if (expirationDay > 31) {
+                        return false
+                    }
+
+                    if (expirationDay >= today.getDate()) {
+                        return true
+                    }
+                }
+
+                case 6: case 6: case 9: case 11: {
+                    if (expirationDay > 30) {
+                        return false
+                    }
+                    if (expirationDay >= today.getDate()) {
+                        return true
+                    }
+                }
+                case 2: {
+                    if ((expirationYear % 400 == 0) || (expirationYear % 4 == 0 && expirationYear % 100 != 0))
+                        if (expirationDay > 29) {
+                            return false
+
+                        } else if (expirationDay > 28) {
+                            return false
+                        }
+
+                    if (expirationDay >= today.getDate()) {
+                        return true
+                    }
+                }
+            }
+        }
+    }
+    console.log('hmmmmmmmmmmmm')
+    return false
+}
+
+function isCardNumberValid(cardNumber) {
+    var value = cardNumber.replace(/\D/g, '');
+    var sum = 0;
+    var shouldDouble = false;
+
+    for (var i = value.length - 1; i >= 0; i--) {
+        var digit = parseInt(value.charAt(i));
+
+        if (shouldDouble) {
+            if ((digit *= 2) > 9) digit -= 9;
+        }
+
+        sum += digit;
+        shouldDouble = !shouldDouble;
+    }
+    return (sum % 10) == 0;
+}
+
+
+function defineCardBrand(cardNumber) {
+    const creditCardsRegex = {
+        visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+        mastercard: /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/,
+        amex: /^3[47][0-9]{13}$/,
+        discover: /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
+        diners_club: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+        jcb: /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/
+    }
+
+    const creditCardPath = {
+        default: '/src/images/branco-logo.jpeg',
+        visa: '/src/images/maestro-logo.png',
+        mastercard: '/src/images/maestro-logo.png',
+        amex: '/src/images/american-express-logo.png',
+        discover: '/src/images/discover-logo.jpg',
+        diners_club: '/src/images/diners-club-logo.jpeg',
+        jcb: '/src/images/jcb-logo.png'
+    }
+
+    if (!isCardNumberValid(cardNumber)) {
+        return {
+            isValid: false,
+            path: creditCardPath.default
+        }
+    }
+
+    let creditCard = {
+        isValid: true,
+        path: creditCardPath.default
+    }
+
+    Object.keys(creditCardsRegex).forEach((key) => {
+        var regex = creditCardsRegex[key];
+
+        if (regex.test(cardNumber)) {
+
+            creditCard.path = creditCardPath[String(key)]
+
+            return creditCard
+        }
+    });
+
+    return creditCard
+}
+
 // Valida entrada do usuário no campo e-mail e informa erro caso tenha inserido algo fora do formato correto
 
 emailInput.addEventListener('blur', (event) => {
@@ -302,3 +448,152 @@ const getAddres = async (zipcode) => {
     zipcodeInput.removeAttribute('disabled')
 
 }
+
+// Verifica qual metodo de pagamento escolhido para mostrar as informações condizentes
+
+paymentMethodSelect.addEventListener('change', (event) => {
+    const selectValue = event.target.value
+
+    if (selectValue === 'credit') {
+
+        creditForm.removeAttribute('hidden')
+        paymentSlip.setAttribute('hidden', 'hiddden')
+
+    } else if (selectValue === 'payment-slip') {
+
+        paymentSlip.removeAttribute('hidden')
+        creditForm.setAttribute('hidden', 'hidden')
+
+    } else {
+
+        creditForm.setAttribute('hidden', 'hidden')
+        paymentSlip.setAttribute('hidden', 'hiddden')
+
+    }
+})
+
+// Limita o usuario a enviar apenas números
+
+cardNumberInput.addEventListener('keypress', (event) => {
+    const olyNumbers = /[0-9]/
+    const key = event.key
+
+    if (!olyNumbers.test(key)) {
+        event.preventDefault()
+
+        return
+    }
+})
+
+// Valida se é um cartão valido e altera logo de bandeira
+
+cardNumberInput.addEventListener('blur', (event) => {
+    const inputValue = String(event.target.value).replace(/[^0-9]/g, '')
+
+    const creditCard = defineCardBrand(inputValue)
+
+
+    if (!creditCard.isValid) {
+        cardNumberInput.setCustomValidity('error')
+        cardCreditLogo.setAttribute('src', creditCard.path)
+    } else {
+        cardNumberInput.setCustomValidity('')
+        cardCreditLogo.setAttribute('src', creditCard.path)
+    }
+})
+
+// Limita o usuario a enviar apenas números
+
+expireInput.addEventListener('keypress', (event) => {
+    const inputValue = event.target.value
+    const olyNumbers = /[0-9]/
+    const key = event.key
+
+    if (!olyNumbers.test(key) || inputValue.length > 9) {
+        event.preventDefault()
+
+        return
+    }
+})
+
+// Coloca '/' conforme a data é colocada
+
+expireInput.addEventListener('keypress', (event) => {
+    const valueInput = event.target.value
+
+    if (valueInput.length === 2 || valueInput.length === 5) {
+        event.target.value += '/'
+    }
+})
+
+// Alerta o usuário caso a data seja de alguma forma invalida, errada ou expirada.
+
+expireInput.addEventListener('blur', (event) => {
+    let valueInput = event.target.value
+
+    const dateInput = valueInput.split('/')
+
+    if (!isDateValid(dateInput)) {
+        expireInput.setCustomValidity('error')
+    } else {
+        expireInput.setCustomValidity('')
+
+    }
+})
+
+// Verifica se o input do usuário é pelo menos duas palavras sendo a primeira de no minimo 2 digitos
+
+nameOnCardInput.addEventListener('blur', (event) => {
+    const inputValue = event.target.value
+    const nameRegex = /^[a-zA-Z]{2,}\ [a-zA-Z]*/
+
+    if (nameRegex.test(inputValue)) {
+        console.log('dentro do if')
+        nameOnCardInput.setCustomValidity('')
+
+        return
+    }
+    console.log('fora do if')
+
+    nameOnCardInput.setCustomValidity('error')
+})
+
+// Limita o usuario a enviar apenas números
+
+securityCodeInput.addEventListener('keypress', (event) => {
+    const inputValue = event.target.value
+    const olyNumbers = /[0-9]/
+    const key = event.key
+
+    if (!olyNumbers.test(key) || inputValue.length > 3) {
+        event.preventDefault()
+
+        return
+    }
+})
+
+// Verifica se tem o formato de CVV valido
+
+securityCodeInput.addEventListener('blur', (event) => {
+    const inputValue = event.target.value
+    const amexCreditCardRegex = /^3[47][0-9]{13}$/
+    const creditCard = cardNumberInput.value
+
+    if (amexCreditCardRegex.test(creditCard)) {
+        if ((/^\d{4}$/).test(inputValue)) {
+            securityCodeInput.setCustomValidity('')
+
+            return
+        } else {
+            securityCodeInput.setCustomValidity('error')
+
+            return
+        }
+    } else if ((/^\d{3}$/).test(inputValue)) {
+        securityCodeInput.setCustomValidity('')
+
+        return
+    }
+
+    securityCodeInput.setCustomValidity('error')
+})
--- star-wars-car-shop/src/routes/Checkout.jsx
+++ star-wars-car-shop/src/routes/Checkout.jsx
@@ -35,7 +35,28 @@ const Checkout = () => {
         <Input type='text' text='UF' name='state' />
       </form>
 
-      <SubmitButton text='Confirmar' />
+      <h2>Dados de pagamento</h2>
+
+      <label htmlFor="paymeny-method">Metodo de pagamento</label>
+      <select name='paymeny-method' id='paymeny-method'>
+        <option value="">Selecione o metodo de pagamento</option>
+        <option value="payment-slip">Boleto</option>
+        <option value="credit">Cartão de crédito</option>
+      </select>
+
+      <form id='credit' hidden>
+        <Input type='text' text='Número do cartão' name='card-number' required='true' errorMessage='Número de cartão invalido' />
+        <img id='card-credit-logo' src="/src/images/branco-logo.jpeg" />
+        <Input type='text' text='Validade' name='expire' placeholder='dd/mm/aaaa' required='true' errorMessage='Validade invalida/expirada' />
+        <Input type='text' text='Nome no cartão:' name='name-on-card' required='true' errorMessage='Envie um nome valido' />
+        <Input type='text' text='CVV' name='security-code' required='true' errorMessage='CVV invalido' />
+      </form>
+
+      <div id="payment-slip" hidden>
+        <p>A confirmação do pagamento ocorrera entre 2-3 dias após o pagamento.</p>
+      </div>
+
+      <SubmitButton text='Confirmar' id='confirm-checkout' />
       <div className="productInfo">
 
       </div>
